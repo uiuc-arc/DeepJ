1c0493511b12394ca6f9a0098d3401cdcab50806
Rem Yang <remyang2@illinois.edu>
2021-11-27 01:26:31 -0600

    Refactor DualIntervals

--- src/DualIntervals.hpp
+++ src/DualIntervals.hpp
@@ -28,29 +28,24 @@ class DualInterval {
     Interval real;
     Interval dual;
 
-    DualInterval(double x1, double x2, double x3, double x4) {
-        this->real = Interval(x1, x2);
-        this->dual = Interval(x3, x4);
+    DualInterval(double rl, double ru, double dl, double du) {
+        real = Interval(rl, ru);
+        dual = Interval(dl, du);
     }
 
-    DualInterval(const Interval& i1, const Interval& i2) {
-        this->real = i1;
-        this->dual = i2;
+    DualInterval(const Interval& ri, const Interval& di) {
+        real = ri;
+        dual = di;
     }
 
-    DualInterval(double x1) {
-        this->real = Interval(x1, x1);
-        this->dual = Interval(0, 0);  // if no dual part is provided, set it to 0
+    DualInterval(double r) {
+        real = Interval(r, r);
+        dual = Interval(0, 0);
     }
 
-    DualInterval(int x1) {
-        this->real = Interval(double(x1), double(x1));
-        this->dual = Interval(0, 0);  // if no dual part is provided, set it to 0
-    }
-
-    DualInterval() {  // if neither a real or dual part is given everything initialized to 0
-        this->real = Interval(0, 0);
-        this->dual = Interval(0, 0);
+    DualInterval() {
+        real = Interval(0, 0);
+        dual = Interval(0, 0);
     }
 
     DualInterval operator+(const DualInterval& rhs) const;
@@ -72,19 +67,18 @@ class DualInterval {
     DualInterval& operator+=(const DualInterval& rhs);
     DualInterval& operator+=(const double rhs);
 
-    // join (union) of intervals
     DualInterval operator|(const DualInterval& rhs) const;
 
-    friend DualInterval sin(const DualInterval& x);
-    friend DualInterval cos(const DualInterval& x);
     friend DualInterval exp(const DualInterval& x);
     friend DualInterval log(const DualInterval& x);
     friend DualInterval sqrt(const DualInterval& x);
     friend DualInterval tanh(const DualInterval& x);
     friend DualInterval atan(const DualInterval& x);
-    friend DualInterval relu(const DualInterval& x);
-    friend DualInterval abs(const DualInterval& x);
     friend DualInterval logistic(const DualInterval& x);
+    friend DualInterval sin(const DualInterval& x);
+    friend DualInterval cos(const DualInterval& x);
+    friend DualInterval abs(const DualInterval& x);
+    friend DualInterval relu(const DualInterval& x);
     friend DualInterval max(const DualInterval& x, const DualInterval& y);
     friend DualInterval min(const DualInterval& x, const DualInterval& y);
 
@@ -101,49 +95,36 @@ class DualInterval {
 
 typedef DualInterval DI;
 
+bool DualInterval::isEmpty() const {
+    return (empty(real) && empty(dual));
+}
+
 void DualInterval::setReal(Interval real_) {
-    this->real = real_;
+    real = real_;
 }
 
 void DualInterval::setReal(double a, double b) {
-    this->real = Interval(a, b);
+    real = Interval(a, b);
 }
 
 void DualInterval::setDual(Interval dual_) {
-    this->dual = dual_;
+    dual = dual_;
 }
 
 void DualInterval::setDual(double a, double b) {
-    this->dual = Interval(a, b);
+    dual = Interval(a, b);
 }
 
 Interval DualInterval::getReal() const {
-    return this->real;
+    return real;
 }
 Interval DualInterval::getDual() const {
-    return this->dual;
-}
-
-bool DualInterval::isEmpty() const {
-    return (empty(real) && empty(dual));
+    return dual;
 }
 
 std::ostream& operator<<(std::ostream& os, const DualInterval& di) {
     os << di.real << " + " << di.dual << "\u03B5";
     return os;
-}
-
-// join operator
-DI DualInterval::operator|(const DI& rhs) const {
-    if (rhs.isEmpty()) {
-        return DI(this->real.lower(), this->real.upper(), this->dual.lower(), this->dual.upper());
-    }
-
-    if (this->isEmpty()) {
-        return DI(rhs.real.lower(), rhs.real.upper(), rhs.dual.lower(), rhs.dual.upper());
-    }
-
-    return DI(hull(this->real, rhs.real), hull(this->dual, rhs.dual));
 }
 
 // internal function only used within this header
@@ -201,12 +182,11 @@ Interval div_intervals(const Interval& a, const Interval& b) {
 
 // addition
 DI DualInterval::operator+(const DI& rhs) const {
-    assert(!this->isEmpty() && !rhs.isEmpty());
+    assert(!isEmpty() && !rhs.isEmpty());
 
-    DI temp;
-    temp.real = add_intervals(this->real, rhs.real);
-    temp.dual = add_intervals(this->dual, rhs.dual);
-    return temp;
+    Interval r = add_intervals(real, rhs.real);
+    Interval d = add_intervals(dual, rhs.dual);
+    return DI(r, d);
 }
 
 // addition with a scalar (this automatically casts the scalar to a DualInterval)
@@ -238,12 +218,11 @@ DI DualInterval::operator-() const {
 
 // multiplication
 DI DualInterval::operator*(const DI& rhs) const {
-    assert(!this->isEmpty() && !rhs.isEmpty());
+    assert(!isEmpty() && !rhs.isEmpty());
 
-    DI temp;
-    temp.real = mul_intervals(this->real, rhs.real);
-    temp.dual = add_intervals(mul_intervals(this->real, rhs.dual), mul_intervals(this->dual, rhs.real));
-    return temp;
+    Interval r = mul_intervals(real, rhs.real);
+    Interval d = add_intervals(mul_intervals(real, rhs.dual), mul_intervals(dual, rhs.real));
+    return DI(r, d);
 }
 
 DI DualInterval::operator*(const double rhs) const {
@@ -256,12 +235,11 @@ DI operator*(const double lhs, const DI& rhs) {
 
 // division
 DI DualInterval::operator/(const DI& rhs) const {
-    assert(!this->isEmpty() && !rhs.isEmpty());
+    assert(!isEmpty() && !rhs.isEmpty());
 
-    DI temp;
-    temp.real = div_intervals(this->real, rhs.real);
-    temp.dual = div_intervals(add_intervals(mul_intervals(this->dual, rhs.real), -mul_intervals(this->real, rhs.dual)), square_interval(rhs.real));
-    return temp;
+    Interval r = div_intervals(real, rhs.real);
+    Interval d = div_intervals(add_intervals(mul_intervals(dual, rhs.real), -mul_intervals(real, rhs.dual)), square_interval(rhs.real));
+    return DI(r, d);
 }
 
 DI DualInterval::operator/(const double rhs) const {
@@ -270,15 +248,28 @@ DI DualInterval::operator/(const double rhs) const {
 
 // increment
 DI& DualInterval::operator+=(const DI& rhs) {
-    assert(!this->isEmpty() && !rhs.isEmpty());
+    assert(!isEmpty() && !rhs.isEmpty());
 
-    this->real = add_intervals(this->real, rhs.real);
-    this->dual = add_intervals(this->dual, rhs.dual);
+    real = add_intervals(real, rhs.real);
+    dual = add_intervals(dual, rhs.dual);
     return *this;
 }
 
 DI& DualInterval::operator+=(double rhs) {
     return *this += DI(rhs);
+}
+
+// join (union)
+DI DualInterval::operator|(const DI& rhs) const {
+    if (rhs.isEmpty()) {
+        return DI(real.lower(), real.upper(), dual.lower(), dual.upper());
+    }
+
+    if (isEmpty()) {
+        return DI(rhs.real.lower(), rhs.real.upper(), rhs.dual.lower(), rhs.dual.upper());
+    }
+
+    return DI(hull(real, rhs.real), hull(dual, rhs.dual));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -290,131 +281,85 @@ DI& DualInterval::operator+=(double rhs) {
 DI exp(const DI& x) {
     assert(!x.isEmpty());
 
-    DI temp;
-    temp.real = exp(x.real);
-    temp.dual = temp.real * x.dual;
-    return temp;
+    Interval r = exp(x.real);
+    Interval d = r * x.dual;
+    return DI(r, d);
 }
 
 DI log(const DI& x) {
     assert(!x.isEmpty());
-
-    DI temp;
-    temp.real = log(x.real);
-    temp.dual = x.dual / x.real;
-    return temp;
+    return DI(log(x.real), x.dual / x.real);
 }
 
 DI sqrt(const DI& x) {
     assert(!x.isEmpty());
 
-    DI temp;
-    temp.real = sqrt(x.real);
-    temp.dual = (x.dual / temp.real) * (0.5);
-    return temp;
+    Interval r = sqrt(x.real);
+    Interval d = x.dual / r * 0.5;
+    return DI(r, d);
+}
+
+DI tanh(const DI& x) {
+    assert(!x.isEmpty());
+
+    Interval r = tanh(x.real);
+    Interval d = (1. - square(r)) * x.dual;
+    return DI(r, d);
+}
+
+DI atan(const DI& x) {
+    assert(!x.isEmpty());
+    return DI(atan(x.real), 1. / (1. + square(x.real)) * x.dual);
+}
+
+DI logistic(const DI& x) {
+    assert(!x.isEmpty());
+
+    Interval t = tanh(x.real / 2.);
+    Interval r = 0.5 * t + 0.5;
+    Interval d = 0.25 * (1. - square(t)) * x.dual;
+    return DI(r, d);
 }
 
 DI sin(const DI& x) {
     assert(!x.isEmpty());
-
-    DI temp;
-    temp.real = sin(x.real);
-    temp.dual = x.dual * cos(x.real);
-    return temp;
+    return DI(sin(x.real), cos(x.real) * x.dual);
 }
 
 DI cos(const DI& x) {
     assert(!x.isEmpty());
-
-    DI temp;
-    temp.real = cos(x.real);
-    temp.dual = -x.dual * sin(x.real);
-    return temp;
+    return DI(cos(x.real), -sin(x.real) * x.dual);
 }
 
-DI relu(const DI& x) {
+DI abs(const DI& x) {
     assert(!x.isEmpty());
 
-    if (x.real.lower() > 0) {
-        return DI(x.real.lower(), x.real.upper(), x.dual.lower(), x.dual.upper());
-    } else if (x.real.upper() < 0) {
-        return DI{};  // empty constructors returns DI(0, 0, 0, 0)
+    if (x.real.upper() < 0) {
+        return -x;
+    } else if (x.real.lower() > 0) {
+        return x;
+    } else {
+        DI positive_branch(0, x.real.upper(), x.dual.lower(), x.dual.upper());
+        DI negative_branch(0, -x.real.lower(), -x.dual.upper(), -x.dual.lower());
+        return (positive_branch | negative_branch);
     }
-
-    // note how we restrict the real part by intersecting it with the set {x : x >0}
-    DI true_branch(0, x.real.upper(), x.dual.lower(), x.dual.upper());
-    DI false_branch{};
-    return (true_branch | false_branch);  // use the join
 }
 
 DI max(const DI& x, const DI& y) {
     assert(!x.isEmpty() && !y.isEmpty());
 
-    DI temp;
     if (x.real.lower() > y.real.upper()) {
-        temp.real = x.real;
-        temp.dual = x.dual;
+        return x;
     } else if (x.real.upper() < y.real.lower()) {
-        temp.real = y.real;
-        temp.dual = y.dual;
-    } else {
-        temp.real = max(x.real, y.real);
-        temp.dual = hull(x.dual, y.dual);
+        return y;
     }
-    return temp;
+    return DI(max(x.real, y.real), hull(x.dual, y.dual));
 }
 
 DI min(const DI& x, const DI& y) {
     return -max(-x, -y);
 }
 
-DI tanh(const DI& x) {
-    assert(!x.isEmpty());
-
-    DI temp;
-    temp.real = tanh(x.real);
-    temp.dual = (1. - square(tanh(x.real))) * x.dual;
-    return temp;
-}
-
-DI atan(const DI& x) {
-    assert(!x.isEmpty());
-
-    DI temp;
-    temp.real = atan(x.real);
-    temp.dual = (1. / (1. + square(x.real))) * x.dual;
-    return temp;
-}
-
-DI logistic(const DI& x) {
-    assert(!x.isEmpty());
-
-    DI temp;
-    temp.real = (0.5 * tanh(x.real / 2.)) + 0.5;
-    temp.dual = (0.25 * (1. - square(tanh(x.real / 2.)))) * x.dual;
-    return temp;
-}
-
-DI abs(const DI& x) {
-    assert(!x.isEmpty());
-
-    if (x.real.upper() < 0.) {
-        DI temp;
-        temp.real = -x.real;
-        temp.dual = -x.dual;
-        return temp;
-    } else if (x.real.lower() > 0.) {
-        DI temp;
-        temp.real = x.real;
-        temp.dual = x.dual;
-        return temp;
-    } else {
-        //note how we restrict the real part by intersecting it with the set {x : x > 0}
-        DI positive_branch(0, x.real.upper(), x.dual.lower(), x.dual.upper());
-
-        //note how we restrict the real part by intersecting it with the set {x : x < 0}
-        Interval neg_branch_dual = -x.dual;
-        DI negative_branch(0, -x.real.lower(), neg_branch_dual.lower(), neg_branch_dual.upper());
-        return (positive_branch | negative_branch);
-    }
+DI relu(const DI& x) {
+    return max(x, 0);
 }
